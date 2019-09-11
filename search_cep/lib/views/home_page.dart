e6cdd81365b566104381f20e2839de7c17c39e8b
Leandro Alves <leandroalves.web@gmail.com>
2019-09-11 12:25:44 -0300

    inserindo botoes, falta corrigir bugs pendentes

--- search_cep/lib/views/home_page.dart
+++ search_cep/lib/views/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:search_cep/models/result_cep.dart';
+import 'package:search_cep/services/via_cep_service.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -12,8 +14,61 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Consultar CEP'),
       ),
-      body: Container(),
-      
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[_buildSearcCepTextField()],
+        ),
+      ),
     );
   }
+
+  var _searchCepController = TextEditingController();
+
+  Widget _buildSearcCepTextField() {
+    return TextField(
+      decoration: InputDecoration(labelText: 'Cep'),
+      controller: _searchCepController,
+    );
+  }
+
+  Widget _buildSearchCepButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: RaisedButton(
+          onPressed: () {
+            _searchCep();
+          },
+          child: Text('Consultar'),
+          shape:
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+      ),
+    );
+  }
+
+  bool _loading = false;  
+  bool _enableField = false;
+
+  void _searching(bool enable) {
+    setState(() {
+      _loading = enable;
+      _enableField = !enable;
+    });
+  }
+
+Widget _circularLoading() {
+  return Container(
+    height: 15.0,
+    width: 15.0,
+    child:  CircularProgressIndicator(),
+  );
+}
+
+Future _searchCep() async {
+  final String cep = _searchCepController.text;
+  final ResultCep = await ViaCepService.fetchCep(cep : cep);
+  print(ResultCep.toJson());
+}
+
 }
